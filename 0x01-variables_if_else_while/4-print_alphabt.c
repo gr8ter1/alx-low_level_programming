818e2c28ebf6f11c33741608ffe8ba1fd8790c24
gr8ter1 <frank4all2k6@gmail.com>
2023-02-17 20:59:52 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'q' || alph == 'e')
+			continue;
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
