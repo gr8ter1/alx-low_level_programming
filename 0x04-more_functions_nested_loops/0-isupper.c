2c3f0d925576626d04edc9370b50769f5aff4bbf
gr8ter1 <frank4all2k6@gmail.com>
2023-02-23 16:27:29 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,14 @@
 /**
  *  _isupper - uppercase letter
  *  @c: char to check
+ *
  *  Return: 0 or 1
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		_putchar (1);
+		return (1);
 	else
-		_putchar (0);
+		return (0);
 }
