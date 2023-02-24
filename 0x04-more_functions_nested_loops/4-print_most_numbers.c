6600ef83593b1d90b3ee1b2f5a11c90d247529a8
gr8ter1 <frank4all2k6@gmail.com>
2023-02-23 17:59:00 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
  */
 void print_most_numbers(void)
 {
-	int c;
+	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	if ((c != 2 && c != 4))
-		_putchar(c + '0');
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }
